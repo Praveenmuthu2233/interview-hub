97fc184d08bb8b6a0a6b9bd16df8b1b1f1a79e3d
Praveenmuthu2233 <praveenmuthu9500@gmail.com>
2026-03-20 15:48:13 +0530

    Firat commit

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB0RlDy45g1XakbW6DpY1FmGCcgG7n8ppg",
+  authDomain: "interview-edge-37ac9.firebaseapp.com",
+  projectId: "interview-edge-37ac9",
+  storageBucket: "interview-edge-37ac9.appspot.com",
+  messagingSenderId: "761868101490",
+  appId: "1:761868101490:web:117d37e8665d4806085feb"
+};
